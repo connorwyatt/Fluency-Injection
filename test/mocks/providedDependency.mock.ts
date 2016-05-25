0d092769b9c439ab5cb0e705ce1269ebaff86b9d
Connor Wyatt <connor.wyatt@midlandhr.co.uk>
2016-05-25 22:49:33 +0100

    Added new useFactory provide method and updated docs

--- test/mocks/providedDependency.mock.ts
+++ test/mocks/providedDependency.mock.ts
@@ -1,5 +1,11 @@
+import { SinonStub, stub } from 'sinon';
+
 export const ProvidedDependencyToken = Symbol('providedDependency');
 
-export const providedDependency = {
+export const valueProvidedDependency = {
   name: 'providedDependency'
 };
+
+export const factoryProvidedDependencyStub: SinonStub = stub();
+
+export const factoryProvidedDependency = {};
